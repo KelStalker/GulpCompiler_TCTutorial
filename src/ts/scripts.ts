45c947a4b6057175c69d6a13260e78540c0d9f23
Kel <kelstalker@hotmail.com>
2020-03-22 15:28:22 -0600

    first commit - includes source scss. ts, and tsconfig pages

---
+++ src/ts/scripts.ts
@@ -0,0 +1,8 @@
+/**
+* Scripts - scripts.ts
+*@date 2020.03.22
+*@author Kel Stalker (workalong code used from Gulp video tutorial in videos folder of TC shared drive
+*/
+
+// Hello World.
+console.log("Hello World!")

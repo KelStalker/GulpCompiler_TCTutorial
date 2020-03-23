d99efcd2742c5ecfaee2b5e2f39d0fb2da2f3ca2
Kel <kelstalker@hotmail.com>
2020-03-22 19:18:42 -0600

    Gulp tutorial complete but 'gulp' command still not recognized

--- gulpfile.js
+++ gulpfile.js
@@ -15,13 +15,24 @@ gulp.task('sass', function () {
 gulp.task('tsc', function () {
     return tsProject.src()
         .pipe(tsProject())
-        .js.pipe(gulp.dest( './' ));
+        .js.pipe(gulp.dest('./'));
     // Note that the destination is simply ./ since we already told it the destination in the var declaration
 
 });
 
 // Default (watch) task
-gulp.task( 'default' );
+// Watches to see if amy ts or scss files are updated and will see them
+// Those changes are noticed and will cause a compilation of the ts/scss files by
+// passing those tasks (above) into the watch 
+gulp.task('default', function () {
+    gulp.watch(
+        ['src/scss/**/.scss', 'src/ts/**/*.ts'],
+        gulp.series(['sass', 'tsc'])
+    );
+});
 
 // Now we have a typescript task that will run the the 
-// a sass task that will compile al of our scss files and compile them into the css/main file
+// a sass task that will compile all of our scss files and compile them into the css folder
+// a ts task taht will compile all of our ts code into the js folder
+
+// **NOTE** To check if all our requirements are in place, run an npm install
